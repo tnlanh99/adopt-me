af42f1e6235c156a2c52946387b0fc4ac939f2e7
tnlanh99 <tnlanh99@gmail.com>
2022-09-27 17:37:52 +0700

    Extract pets list to Results component

---
+++ src/Results.js
@@ -0,0 +1,25 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            id={pet.id}
+            key={pet.id}
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
