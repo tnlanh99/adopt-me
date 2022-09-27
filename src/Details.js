ec5a7ab2f8a7ad78a2ae52f6e5e765cfcabe97c7
tnlanh99 <tnlanh99@gmail.com>
2022-09-27 17:44:48 +0700

    Implement fetching data for Details component

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-    };
+  state = { loading: true };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+    this.setState({ loading: false, ...json.pets[0] });
   }
 
   render() {
@@ -13,7 +17,7 @@ class Details extends React.Component {
       return <h2>Loading...</h2>;
     }
 
-    const { name, animal, breed, city, state, description } = this.props;
+    const { name, animal, breed, city, state, description } = this.state;
 
     return (
       <div className="details">
@@ -30,4 +34,9 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+const WrapperDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrapperDetails;
