ac8b58b53accda7f1d385c96a09b37da1b6d1214
tnlanh99 <tnlanh99@gmail.com>
2022-10-03 15:38:45 +0700

    Use Modal portal in Details component

--- src/Details.js
+++ src/Details.js
@@ -3,9 +3,12 @@ import { useParams } from "react-router-dom";
 import ThemeContext from "./ThemeContext";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 class Details extends React.Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
 
   async componentDidMount() {
     const res = await fetch(
@@ -20,7 +23,7 @@ class Details extends React.Component {
       return <h2>Loading...</h2>;
     }
 
-    const { name, animal, breed, city, state, description, images } =
+    const { name, animal, breed, city, state, description, images, showModal } =
       this.state;
 
     return (
@@ -31,10 +34,24 @@ class Details extends React.Component {
           <h2>
             {animal} - {breed} - {city}, {state}
           </h2>
-          <button style={{ backgroundColor: this.props.themeColor }}>
+          <button
+            onClick={this.toggleModal}
+            style={{ backgroundColor: this.props.themeColor }}
+          >
             Adopt {name}
           </button>
           <p>{description}</p>
+          {showModal && (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <a href="https://bit.ly/pet-adopt">Yes</a>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          )}
         </div>
       </div>
     );
